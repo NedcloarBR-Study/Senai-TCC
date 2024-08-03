eb3a7d364a58d9ad4b8c199468086cc6bcd771b6
NedcloarBR <nedcloar1@hotmail.com>
2024-08-03 14:31:33 -0300

    feat: configure database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { PrismaModule } from "nestjs-prisma";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+		PrismaModule.forRoot({
 			isGlobal: true,
 		}),
 	],
