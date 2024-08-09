bb4559e2b0b299f8eca5ba5b02cb9526b09a31aa
NedcloarBR <nedcloar1@hotmail.com>
2024-08-09 00:15:40 -0300

    test: test

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -212,7 +212,6 @@ describe("UserModule Tests", () => {
 		}
 		prismaMock.user.findMany.mockResolvedValue(mockReturn);
 		prismaMock.user.count.mockResolvedValue(10);
-
 		const result = await userController.list();
 		const expected = {
 			count: users.length,
