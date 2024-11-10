e4ae4fd6ea6d4a1e29bee39646e10de0a1eef09d
NedcloarBR <nedcloar1@hotmail.com>
2024-11-10 18:04:54 -0300

    docs(api/auth): add swagger

--- apps/api/src/modules/auth/auth.controller.ts
+++ apps/api/src/modules/auth/auth.controller.ts
@@ -8,21 +8,36 @@ import {
 	Res,
 	UseGuards,
 } from "@nestjs/common";
+import {
+	ApiBody,
+	ApiFoundResponse,
+	ApiNotFoundResponse,
+	ApiResponse,
+	ApiTags,
+} from "@nestjs/swagger";
 import type { FastifyReply } from "fastify";
 import { AuthUser } from "src/common/decorators/AuthUser.decorator";
 import { JwtAuthGuard } from "src/common/guards/jwt.guard";
 import { Routes, Services } from "src/types/constants";
 import type { IAuthService } from ".";
-// biome-ignore lint/style/useImportType: <Cannot useImportType on DTOs>
 import { type UserEntity, UserLoginDTO } from "../user";
 
 @Controller(Routes.Auth)
+@ApiTags(Routes.User)
 export class AuthController {
 	public constructor(
 		@Inject(Services.Auth) private readonly authService: IAuthService,
 	) {}
 
 	@Post("login")
+	@ApiBody({
+		type: UserLoginDTO,
+	})
+	@ApiFoundResponse({ status: HttpStatus.FOUND, description: "User Logged" })
+	@ApiNotFoundResponse({
+		status: HttpStatus.NOT_FOUND,
+		description: "No User found with this credentials",
+	})
 	public async login(
 		@Body() userLoginDTO: UserLoginDTO,
 		@Res() res: FastifyReply,
@@ -34,6 +49,7 @@ export class AuthController {
 
 	@Get("status")
 	@UseGuards(JwtAuthGuard)
+	@ApiResponse({ status: HttpStatus.OK, description: "User status" })
 	public status(@AuthUser() user: UserEntity): UserEntity {
 		return user;
 	}
